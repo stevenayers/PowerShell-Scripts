5d5f9f5e018bbfb2f4cb9e3f9bca018ad060416f
StevenAyers <steveay99@gmail.com>
2016-07-16 19:38:46 +0100

    Add Get-AzureRmVMIP to Random Functions

---
+++ Random Functions/Get-AzureRMVMIP.ps1
@@ -0,0 +1,22 @@
+﻿function Get-AzureRmVMIP {
+    $vms = Get-AzureRMVM
+    $ReturnObject = @()
+    $vmcount = 0
+    foreach ($vm in $vms)
+    {
+        Write-Progress -PercentComplete (($vmcount / $vms.Count) * 100) -Activity "Gathering Public IP Addresses..." -Status "$($vm.Name)..."
+
+        $IPConfig = (Get-AzureRmNetworkInterface | ? { $_.Id -eq $vm.NetworkInterfaceIDs}).IpConfigurations
+
+
+        $ReturnObject +=  New-Object -TypeName PSObject -Property @{
+            Name=$vm.Name;
+            PublicIPAddress=(Get-AzureRmPublicIpAddress | ? { $_.Id -eq $IPConfig.PublicIPAddress.Id}).IpAddress;
+            PrivateIPAddress=$IPConfig.PrivateIPAddress
+            } | Write-Output
+
+        $vmcount++
+    }
+
+    return $ReturnObject
+}
