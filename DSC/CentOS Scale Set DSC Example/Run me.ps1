bf0ee71823cc0f00640121caf1156e986e0cd70a
StevenAyers <steveay99@gmail.com>
2016-07-16 21:45:49 +0100

    Add Linux Scale Set Example

---
+++ DSC/CentOS Scale Set DSC Example/Run me.ps1
@@ -0,0 +1,39 @@
+﻿#$Credential = Get-Credential
+$i = "2"
+$ResourceGroupName = "dsclinuxgroup$i"
+$AccountName = "Autodsc$i"
+$DomainNamePrefix = "dscbase$i"
+$ResourceLocation = 'West Europe'
+$VirtualMachineScaleSetName = "bl$i"
+$InstanceCount = 2
+
+New-AzureRmResourcegroup -Name $ResourceGroupName -Location 'West Europe' -Verbose
+
+New-AzureRMAutomationAccount -ResourceGroupName $ResourceGroupName -Name $AccountName -Location 'West Europe' -Plan Free -Verbose
+
+$RegistrationInfo = Get-AzureRmAutomationRegistrationInfo -ResourceGroupName $ResourceGroupName -AutomationAccountName $AccountName
+$jid = ([system.guid]::newguid().guid).ToString()
+$RegKey = $RegistrationInfo.PrimaryKey | ConvertTo-SecureString -AsPlainText -Force
+
+#############
+$Params=@{
+
+    "registrationKey"=$RegistrationInfo.PrimaryKey;
+    "registrationUrl"=$RegistrationInfo.Endpoint;
+    "automationAccountName"=$AccountName;
+    "jobid"=$jid;
+    "adminUsername"=$credential.UserName;
+    "adminPassword"=$credential.Password;
+    "domainNamePrefix"=$DomainNamePrefix;
+    "resourceLocation"=$ResourceLocation;
+    "vmssName"=$VirtualMachineScaleSetName;
+    "instanceCount"=$InstanceCount;
+    "timestamp"=((get-date).getdatetimeformats()[80]);
+
+}
+
+New-AzureRmResourceGroupDeployment  -Name TestDeployment1 `
+                                    -ResourceGroupName $ResourceGroupName `
+                                    -TemplateFile "$PSScriptRoot\azuredeploy.json" `
+                                    -TemplateParameterObject $Params `
+                                    -Verbose
---
+++ DSC/CentOS Scale Set DSC Example/baseline.ps1
@@ -0,0 +1,12 @@
+﻿configuration InstallBaseline {
+
+    Import-DscResource nx
+    node Linux.Baseline {
+        nxPackage nano {
+            Name = 'nano'
+        }
+        nxPackage httpd {
+            Name = 'httpd'
+        }
+    }
+}
