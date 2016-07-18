f689ef1d44c80f4603f08d015bfb6966ebecc5d4
Stevea37 <steveay99@gmail.com>
2016-07-18 10:28:44 +0100

    Add Elevate-PowerShell

---
+++ Random Functions/Elevate-PowerShell.ps1
@@ -0,0 +1,55 @@
+﻿function Elevate-PowerShell {
+	[CmdletBinding()]
+param(
+	[parameter(Mandatory=$false)][switch]$ISE
+)
+if ((get-host).Name -ilike "*ISE*")
+{
+    $CurrentISE = $true
+    $PS = "PowerShell_ISE"
+}
+else
+{
+    $CurrentISE = $false
+    if ($ISE)
+    {
+    	$PS = "PowerShell_ISE"
+    }
+    else
+    {
+    	$PS = "PowerShell"
+    }
+}
+
+# ELEVATION
+    $user=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $userpcl=new-object System.Security.Principal.WindowsPrincipal($user)
+    $admin=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+        if ($userpcl.IsInRole($admin)) {
+        clear-host
+            if ($ISE)
+            {
+                if ($CurrentISE)
+                {
+                    Write-host "You are already using PowerShell ISE as Administrator." -ForegroundColor Gray
+                }
+                else
+                {
+                    ise
+                    exit
+                }
+            }
+            
+        }
+        else {
+            $newProcess = new-object System.Diagnostics.ProcessStartInfo "$PS";
+            $newProcess.Verb = "runas";
+            [System.Diagnostics.Process]::Start($newProcess);
+            exit
+        }
+
+
+
+}
+
+New-Alias elv Elevate-PowerShell
